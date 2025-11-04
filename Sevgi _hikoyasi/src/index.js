7d133eba7fa9d26d74225b69712dca84a7d655d7
Ziyodxon005 <lutfiddinovziyodxon45@gmail.com>
2025-11-05 04:43:30 +0500

    Delete Sevgi _hikoyasi directory

--- Sevgi _hikoyasi/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import LoveChoicesGame from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<LoveChoicesGame />);
